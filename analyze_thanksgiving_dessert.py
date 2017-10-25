6ead42d894c51f59aad698b3665ab9358148f044
aautade <33081401+aautade@users.noreply.github.com>
2017-10-25 13:46:01 +0530

    Create analyze_thanksgiving_dessert.py

---
+++ analyze_thanksgiving_dessert.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data = pd.read_csv("thanksgiving.csv",encoding="Latin-1")
+cols = data.columns.tolist()
+cols_dessert = []
+for each in cols:
+    if each.startswith("Which of these dessert"):
+        cols_dessert.append(each)
+dessert = {}
+for each in cols_dessert:
+    dessert_type = each.split("- ")[1]
+    dessert_value = data[each].value_counts().tolist()
+    #print(dessert_value[0])
+    dessert[dessert_type] = dessert_value[0]
+print(dessert)
